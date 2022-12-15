a545eba08b7c3590d6bb5e0e6a8337dd28331432
darikmohammed <r.m.darik@gmail.com>
2022-12-15 22:25:43 +0300

    Create a migration for teacher

---
+++ db/migrate/20221215184800_create_teachers.rb
@@ -0,0 +1,15 @@
+class CreateTeachers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.integer :age
+      t.string :profession
+      t.text :location
+      t.references :subject, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
