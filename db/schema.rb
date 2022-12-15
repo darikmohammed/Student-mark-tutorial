bbfa8c903b972ec30c1e4f4c2969e8d0a2b49f0f
darikmohammed <r.m.darik@gmail.com>
2022-12-15 22:54:51 +0300

    Add student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_195157) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_195425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_195157) do
     t.string "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "phone"
+    t.string "email"
+    t.bigint "semester_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_students_on_room_id"
+    t.index ["semester_id"], name: "index_students_on_semester_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -49,5 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_195157) do
     t.index ["subject_id"], name: "index_teachers_on_subject_id"
   end
 
+  add_foreign_key "students", "rooms"
+  add_foreign_key "students", "semesters"
   add_foreign_key "teachers", "subjects"
 end
