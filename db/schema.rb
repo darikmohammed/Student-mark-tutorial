89d4b3fbb04dd6b001b84e7e47bbece8e9e19df6
darikmohammed <r.m.darik@gmail.com>
2022-12-15 22:51:08 +0300

    Add teacher model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_193936) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_194943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,4 +29,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_193936) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "phone"
+    t.string "email"
+    t.string "profession"
+    t.text "location"
+    t.bigint "subject_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_teachers_on_subject_id"
+  end
+
+  add_foreign_key "teachers", "subjects"
 end
