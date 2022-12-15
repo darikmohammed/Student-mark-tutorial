fadabc1ae638524a67ce5bdae7725d8c2eb5e557
darikmohammed <r.m.darik@gmail.com>
2022-12-15 22:36:52 +0300

    Create a semester model

--- app/models/student.rb
+++
@@ -1,4 +0,0 @@
-class Student < ApplicationRecord
-  belongs_to :semester
-  belongs_to :class
-end
--- app/models/teacher_class.rb
+++
@@ -1,4 +0,0 @@
-class TeacherClass < ApplicationRecord
-  belongs_to :class
-  belongs_to :teacher
-end
